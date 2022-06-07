cd8f4ad1b65341a878d8eb80a200a6bc4e22b0c9
MrTima01 <106863228+MrTima01@users.noreply.github.com>
2022-06-07 09:49:58 -0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -5,3 +5,62 @@ NEOTROX - TEENUHX
 */
 
 
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function whatsAsena() {
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [2, 2143, 3];
+
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('DARK')}${chalk.blue.bold('MAX')}
+${chalk.white.italic('DarkMaxString')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
+
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('DarkMax QR Code: '),
+			'' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo()))
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('90')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️\n\npowerd by dark max',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️\n\npwoerd by dark max',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!\nඔබට කොපි කර ගැනීමට නොහැකි නම් ඔබගේ නම්බර් එකට පණිවිඩයක් මගින් කේතය ලැබේ"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!\nඔබට කොපි කර ගැනීමට නොහැකි නම් ඔබගේ නම්බර් එකට පණිවිඩයක් මගින් කේතය ලැබේ'
+			)
+		);
+		process.exit(0);
+	});
+
+	await conn.connect();
+}
+
+whatsAsena();
